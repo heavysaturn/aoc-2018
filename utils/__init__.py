6c94f9258663051ccdf81a52e09a3af0278b9974
Leon Sandøy <leon.sandoy@conmodo.com>
2018-12-06 12:57:51 +0100

    timeit recipe

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,6 @@
 def load_input(input_file = "input.txt"):
     with open(input_file, "r") as readfile:
         return readfile.read().strip().split("\n")
+
+# TIMEIT #
+# timeit.timeit("function(args)", globals=globals(), number=number_of_times_to_test)
