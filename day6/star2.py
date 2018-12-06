670213db135d760fc8c87f1de7f7517842d954fc
Leon Sandøy <leon.sandoy@conmodo.com>
2018-12-06 13:02:04 +0100

    improved colors on the viz

--- day6/star2.py
+++ day6/star2.py
@@ -1,26 +1,30 @@
+import timeit
 from operator import itemgetter
 
 from day6.grid import ChronalGrid
 from utils import load_input
 
-chronal_coords = load_input()
+def star2():
+    chronal_coords = load_input()
 
-# Convert input to a list of tuples
-tuple_coords = []
-for coord in chronal_coords:
-    x, y = coord.split(", ")
-    tuple_coords.append((int(x), int(y)))
+    # Convert input to a list of tuples
+    tuple_coords = []
+    for coord in chronal_coords:
+        x, y = coord.split(", ")
+        tuple_coords.append((int(x), int(y)))
 
-# Figure out the necessary height and width
-width = max(tuple_coords, key=itemgetter(0))[0] + 2
-height = max(tuple_coords, key=itemgetter(1))[1] + 1
+    # Figure out the necessary height and width
+    width = max(tuple_coords, key=itemgetter(0))[0] + 2
+    height = max(tuple_coords, key=itemgetter(1))[1] + 1
 
-print(f"width: {width}")
-print(f"height: {height}")
+    print(f"width: {width}")
+    print(f"height: {height}")
 
-# Initiate the grid
-grid = ChronalGrid(width, height)
-grid.process_coords(tuple_coords)
+    # Initiate the grid
+    grid = ChronalGrid(width, height)
+    grid.process_coords(tuple_coords)
 
-# Print the largest area for a finite destination
-print(grid.get_largest_safe_area())
+    # Print the largest area for a finite destination
+    print(grid.get_largest_safe_area())
+
+print(timeit.timeit("star2()", globals=globals(), number=1))
