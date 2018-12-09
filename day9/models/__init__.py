f265c4432078cd141dc285b3ad322edf9f9df2b1
Leon Sandøy <leon.sandoy@conmodo.com>
2018-12-09 13:10:35 +0100

    Optimization: No Elf or Marble classes, fewer helper methods. Method calls are slow.

--- day9/models/__init__.py
+++ day9/models/__init__.py
@@ -1,5 +1,3 @@
 from .circle import Circle
-from .elf import Elf
-from .marble import Marble
 
-__all__ = ["Circle", "Elf", "Marble"]
+__all__ = ["Circle"]
--- day9/models/circle.py
+++ day9/models/circle.py
@@ -1,8 +1,4 @@
 from collections import deque
-from operator import attrgetter
-
-from day9.models.elf import Elf
-from day9.models.marble import Marble
 
 
 class Circle:
@@ -13,82 +9,56 @@ class Circle:
 
     def __init__(self, players, target, log=False):
         self.log = log
-        self.marbles = deque()
+        self.marbles = deque([0])
         self.target_marbles = target
-        self.players = deque([Elf(self) for _ in range(players)])
+        self.players = [0 for _ in range(players)]
         self.marbles_played = 0
 
     def __repr__(self):
         marbles = ""
-        for index, marble in enumerate(self.marbles):
-            marbles += f"{marble.id} "
+        deq = self.marbles.copy()
+        index = deq.index(0)
+        deq.rotate(-index)
+
+        for index, marble in enumerate(deq):
+            if marble == self.marbles[0]:
+                marbles += f"({marble}) "
+            else:
+                marbles += f"{marble} "
         return marbles
-
-    def place_marble(self, marble):
-        """
-        Place a marble in the circle.
-        """
-        self.marbles.rotate(-1)
-        self.marbles.append(marble)
-
-    def get_player(self):
-        """
-        Gets the next player whose
-        turn it is.
-        """
-        index = (self.marbles_played - 1) % len(self.players)
-        return self.players[index]
 
     def get_final_score(self):
         """
         Get the player who has the highest score.
         """
-        player = max(self.players, key=attrgetter("score"))
-        return player.score
-
-    def remove_marble(self):
-        """
-        Removes the marble 7 marbles counter-clockwise
-        from the current marble, and returns it.
-
-        Sets the marble clockwise of that one as the
-        new current marble.
-        """
-
-        # Rotate 7 times counter clockwise and then remove that item
-        self.marbles.rotate(7)
-        marble = self.marbles.pop()
-
-        # Rotate back so the marble clockwise of the one we removed is the current marble.
-        self.marbles.rotate(-1)
-
-        # Return the marble we removed.
-        return marble
+        return max(self.players)
 
     def play(self):
-
-        # First place a single marble in the circle.
-        first_marble = Marble()
-        self.place_marble(first_marble)
 
         if self.log:
             print(self)
 
         # Play until the target marble number is reached.
-        while self.marbles_played != self.target_marbles:
-            marble = Marble()
+        for marble in range(1, self.target_marbles + 1):
 
             # If the marble is a multiple of 23, score points!
-            if marble.id % 23 == 0:
-                player = self.get_player()
-                removed_marble = self.remove_marble()
-                player.score += marble.id
-                player.score += removed_marble.id
+            if marble % 23 == 0:
 
+                # find the player index
+                player_index = marble % len(self.players)
+
+                # Rotate 7 times counter clockwise and then remove that item
+                self.marbles.rotate(7)
+                removed_marble = self.marbles.popleft()
+
+                # Add the score
+                self.players[player_index] += marble
+                self.players[player_index] += removed_marble
+
+            # Otherwise, just rotate and add the marble to the deque
             else:
-                self.place_marble(marble)
-
-            self.marbles_played += 1
+                self.marbles.rotate(-2)
+                self.marbles.appendleft(marble)
 
             if self.log:
                 print(self)
